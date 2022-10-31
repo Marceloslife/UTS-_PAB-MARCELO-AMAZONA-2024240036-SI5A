39f3d87937789cc6ba65e16099f2b9728223a20f
Marceloslife <113509452+Marceloslife@users.noreply.github.com>
2022-10-31 19:20:54 +0700

    Lets go

--- app/src/main/java/com/si5a/uts_2024240036/MainActivity.java
+++ app/src/main/java/com/si5a/uts_2024240036/MainActivity.java
@@ -1,32 +1,58 @@
 package com.si5a.uts_2024240036;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    private Spinner spinner;
+public class MainActivity extends AppCompatActivity {
+private EditText etNamaLengkap,etNp;
+private CheckBox checkBoxmeat;
+private Spinner sp1;
+private Button btn;
+private String nama,nomorpendaftaran,spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner = findViewById(R.id.spinner1);
+        etNamaLengkap = findViewById(R.id.et_nama_lengkap);
+        etNp          = findViewById(R.id.et_np);
+        checkBoxmeat  = findViewById(R.id.checkbox_meat);
+        sp1       = findViewById(R.id.spinner1);
+        btn      = findViewById(R.id.btn_daftar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            nama = etNamaLengkap.getText().toString();
+            nomorpendaftaran = etNp.getText().toString();
+            spinner          =  sp1.getSelectedItem().toString();
+            if(nomorpendaftaran.trim().equals("")){
+                etNp. setError("Set error!");
+            }
+            else if (nama.trim().equals("")){
+                etNamaLengkap. setError("Set error!");
+            }
+            else if (spinner.trim().equals("jalur Pendaftaran")){
+                Toast.makeText(MainActivity.this, "pilih jalur pendaftaran!", Toast.LENGTH_SHORT).show();
+            }
+            else if (!(checkBoxmeat.isChecked())){
+                    Toast.makeText(MainActivity.this, "check box belum dicentang", Toast.LENGTH_SHORT).show();
+                }
+            else {
+                Intent intent = new
+                        Intent(MainActivity.this, Second.class);
+                startActivity(intent);
+                }
+            }
+        });
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.text, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
-        spinner.setOnItemClickListener(this);
 
-    }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        String choice = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(getApplicationContext(),choice,Toast.LENGTH_LONG).show();
     }
 }
